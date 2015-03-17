79045868a68490a18a1af5b0a8b67a3b01fbb882
Qianfinland <qianfinland@gmail.com>
2015-03-17 23:26:14 +0200

    add searchkick

--- Gemfile
+++ Gemfile
@@ -28,4 +28,5 @@ gem 'spring',        group: :development
 gem 'devise', '~> 3.4.1'
 gem 'paperclip', '~> 4.2.1'
 gem 'bootstrap-sass', '~> 3.3.3'
+gem 'searchkick', '~> 0.8.7'
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :movies do
+    collection do
+      get 'search'
+    end
     #nested views, use rake route
     resources :reviews, except: [:show, :index]
   end
